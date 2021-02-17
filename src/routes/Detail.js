94194a512594fa968f12a3aa7800db4f7cb67ccb
Jungwon Goda <koag132@gmail.com>
2021-02-16 16:25:14 -0800

    Added useParams and got the id on Details.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,9 @@
-export default () => "Detail";
+import React from 'react';
+import {useParams} from 'react-router-dom';
+
+//we can get parameters using useParams()
+export default () => {
+    const {id} = useParams();
+    console.log(id)
+    return "Detail";
+};
