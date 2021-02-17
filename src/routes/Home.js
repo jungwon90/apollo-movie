16b929acfe1c463b10605411af36fbe2c613a1d7
Jungwon Goda <koag132@gmail.com>
2021-02-16 16:20:12 -0800

    Added Movie components + styled-components

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {gql, useQuery} from '@apollo/client';
+import styled from 'styled-components';
+import Movie from '../components/Movie';
+
 
 //create queries outside the components
 const GET_MOVIES = gql`
@@ -11,16 +14,63 @@ const GET_MOVIES = gql`
     }
 `;
 
+//Define Container component using styled-component
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%
+`;
+
+//Define Header component using styled-component
+const Header = styled.header`
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    height: 45vh;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`;
+
+//Define Title component using styled-component
+const Title = styled.h1`
+    font-size: 60px;
+    font-weight: 600;
+    margin-bottom: 20px;
+`;
+
+//Define Subtitle component using styled-component
+const Subtitle = styled.h3`
+    font-size: 35px;
+`;
+
+//Define Loading component using styled-component
+const Loading = styled.div`
+    font-size: 18px;
+    opacity: 0.5;
+    font-weight: 500;
+    margin-top: 10px;
+`;
+
 export default () => {
     //useQuery requires a query 
     const { loading, error, data } = useQuery(GET_MOVIES);
     console.log(loading, error, data);
     
-    if(loading){
-        return 'loading...';
-    }
-    if(data && data.movies){
-        return data.movies.map(movie => <h1>{movie.id}</h1>);
-    }
-    
+    /**
+    <Container> -> The container centers your content horizontally. 
+    It's the most basic layout element.
+     */
+    return(
+        <Container>
+            <Header>
+                <Title>Apollo Movie</Title>
+                <Subtitle>I Love GraphQL</Subtitle>
+            </Header>
+            {loading && <Loading>Loading...</Loading>}
+            {!loading && data.movies && data.movies.map(movie => <Movie key={movie.id} id={movie.id}/>)}
+        </Container>
+    );  
 };
