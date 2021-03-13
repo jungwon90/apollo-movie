2e64f653a637d19f504b3429ea8f8f52796fea4c
Jungwon Goda <koag132@gmail.com>
2021-03-12 17:40:44 -0800

    Added Mutation of toggleLikeMovie

--- src/apollo.js
+++ src/apollo.js
@@ -1,13 +1,25 @@
-import {ApolloClient, InMemoryCache} from "@apollo/client";
+import {ApolloClient, InMemoryCache, gql} from "@apollo/client";
 // we need to wrap our React app with this client
 const client = new ApolloClient({
     uri: 'http://localhost:4000/',
     cache: new InMemoryCache(),
     resolvers: {
         //added isLiked inside the Movie type that we get from backend, it will be stored in cache.
-        //combining data from back-end and data from fron-end
         Movie:{
             isLiked: () => false
+        },
+        //We can also define mutations on Apollo-graphql Client
+        Mutation: {              //pass the cache object to get data from cache
+            toggleLikeMovie: (_, {id}, {cache}) => {
+                //we want to use writeData() of cache object to modify 'isLiked'
+                cache.modify({
+                    id: `Movie:${id}`,
+                    fields: {
+                        isLiked: (isLiked) => !isLiked,
+                    },
+                });
+                console.log(cache);
+            }
         }
     }
 });
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
+import {gql, useMutation} from '@apollo/client';
+
+//mutation likeMovie(id: $id) @client 
+//-> need to tell Apollo that this mutation is on client
+//because we don't want to send this mutation to the backend
+const LIKE_MOVIE = gql`
+    mutation toggleLikeMovie($id: Int!){
+        toggleLikeMovie(id: $id) @client
+    }
+`;
 
 const Container = styled.div`
-    height: 380px;
+    height: 400px;
+    border-radius: 7px;
     width: 100%;
     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    overflow: hidden;
-    border-radius: 7px;
+    background-color: transparent;
 `;
     
 const Poster = styled.div`
@@ -16,14 +26,20 @@ const Poster = styled.div`
     width: 100%;
     background-size: cover;
     background-position: center center;
+    border-radius: 7px;
 `;
 
 
-export default ({id, bg, isLiked}) => (
-    <Container>
-        <Link to={`/${id}`}>
-            <Poster bg={bg}/>
-        </Link>
-        <button>{isLiked ? 'Unlike' : 'Like'}</button>
-    </Container>
-);
+export default ({id, bg, isLiked}) => {
+    //useMutation() will give me a mutation as the first element in the array.
+    const [toggleLikeMovie] = useMutation(LIKE_MOVIE, {variables: {id: parseInt(id)}})
+    return (
+        <Container>
+            <Link to={`/${id}`}>
+                <Poster bg={bg}/>
+            </Link>
+            <button onClick={toggleLikeMovie}>{isLiked ? "Unlike" : "Like"}</button>
+        </Container>
+    );
+};
+
